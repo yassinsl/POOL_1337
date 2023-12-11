88e8fc5fed5d93a8d0996fa5dd6977d1d46be88c
YASSINE LAHSSINI <slawiyassin152@gmail.com>
2023-12-11 11:07:57 +0100

    POOL 1337

---
+++ DAYS_03/ex04/ft_strstr.c
@@ -0,0 +1,28 @@
+char	*ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int j;
+
+	i = 0;
+	if (to_find[0] == '\0')
+		return (str);
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] != '\0' && str[i + j] == to_find[j])
+		{
+			if (to_find[j + 1] == '\0')
+				return (&str[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+int main (void)
+{
+	char a[] = "my name is yassin lahssini i m 19 years old ";
+	char b[] = "yassin";
+	printf("%s\n",ft_strstr(a,b));
+	return (0);
+}

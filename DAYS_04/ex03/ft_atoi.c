971130e40f510c1969448b3bc52244870c086d9c
YASSINE LAHSSINI <slawiyassin152@gmail.com>
2023-12-15 15:21:50 +0100

    POOL 1337

--- DAYS_04/ex03/ft_atoi.c
+++ DAYS_04/ex03/ft_atoi.c
@@ -2,23 +2,25 @@ int ft_atoi(char *str)
 {
 	int i = 0 ,j = 0,negative = 0;
 		while (str[i] && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' ||
-			str[i] == '\v' || str[i] == '\f' || str[i] == '\r'))
+			str[i] == '\v' || str[i] == '\f' || str[i] == '\r')) // 9 -> 13 || 32//
 		++i;
-	if (str[i] == '-') 
-	{
-        negative = 1;
-        ++i;
-    } 
-	else if (str[i] == '+') 
-        ++i;
-	
+		if(str[i] == '-')
+		{
+			negative++;
+			i++;
+		}
+		else if(str[i] == '+')
+		i++;
+
 	while(str[i])
 	{
 		if(str[i] >= '0' && str[i] <= '9')
 		j = j * 10 + str[i] - '0';
+	else if(str[i] >= 'a' && str[i] <= 'z'|| str[i] >= 'A' && str[i] <= 'Z')
+			break;
 		i++;
 	}
-	 if (negative) 
+	 if (negative != 0) 
         return -j;
 	else 
         return j;
@@ -26,7 +28,7 @@ int ft_atoi(char *str)
 #include <stdio.h>
 int main (void)
 {
-	char a[] = "--1234ab567";
+	char a[] ="    		111111--------1234ab567";
 	int b = ft_atoi(a);
 	printf("%d\n",b);
 
